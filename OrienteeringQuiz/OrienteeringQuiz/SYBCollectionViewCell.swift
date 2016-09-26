9866fe9b1a9dcaba6f12c96e91cde73cf95dd3b6
stefy83 <stefy83@gmail.com>
2016-09-26 21:45:20 +0200

    Rendeo las esquinas de las imágenes, queda mas bonito

--- OrienteeringQuiz/OrienteeringQuiz/SYBCollectionViewCell.swift
+++ OrienteeringQuiz/OrienteeringQuiz/SYBCollectionViewCell.swift
@@ -12,6 +12,23 @@ class SYBCollectionViewCell: UICollectionViewCell {
 
     
     @IBOutlet weak var myImagenSimbolo: UIImageView!
+    @IBOutlet weak var myImagenTickOK_KO: UIImageView!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        //Redondeamos las esquinas de las imágenes
+        self.makeItRounded()
+        
+        myImagenTickOK_KO.hidden = true
+    }
+    
+    func makeItRounded() {
+        self.myImagenSimbolo.layer.masksToBounds = true
+        self.myImagenSimbolo.layer.cornerRadius = 10
+        self.myImagenTickOK_KO.layer.masksToBounds = true
+        self.myImagenTickOK_KO.layer.cornerRadius  = 10
+    }
 
 
     
